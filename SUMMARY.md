a1f75f61738536455c4108a4e9d35dbec60e7d17
Flamme Shadow <FlammeShadow@outlook.com>
2024-07-21 00:12:15 +0800

    添加简单的多语言支持。

--- SUMMARY.md
+++ SUMMARY.md
@@ -1,9 +1,20 @@
 # Summary
 
 [zh-CN](./zh-CN/contents.md)
+[en-US](./en-US/contents.md)
 
 ## zh-CN
 
-- [](./zh-CN/about.md)
-- [](./zh-CN/bibliography.md)
-- [](./zh-CN/plt/about.md)
+# 须知
+
+- [关于本存储库](./zh-CN/about.md)
+
+# 纲要
+
+- [关于编程语言理论](./zh-CN/plt/about.md)
+
+# 术语和文献
+
+- [参考文献](./zh-CN/bibliography.md)
+
+## en-US
--- index.js
+++ index.js
@@ -1,14 +1,59 @@
-let hideEmptyChapterItems = () => {
-    let chapterItems = document.getElementsByClassName("chapter-item expanded");
+const supportLanguages = ["zh-CN", "en-US"];
 
-    Array.from(chapterItems).forEach(item => {
-        let text = item.firstChild.textContent;
-        if (text.match(/[1-9]*\./)) {
+let languageChapters = {
+    "zh-CN": [], "en-US": []
+};
+
+let currentLanguage;
+
+for (let language of supportLanguages) {
+    if (document.baseURI.includes(`/${language}/`)) {
+        currentLanguage = language;
+        break;
+    }
+}
+
+let hideEmptyChapterItems = () => {
+    let chapterItems = Array.from(document.getElementsByClassName("chapter-item expanded"));
+
+    chapterItems.findIndex(item => {
+        if (item.classList.contains("affix")) { return; }
+
+        let child = item.firstElementChild;
+
+        if (child.nextSibling == null) {
+            if (item.previousElementSibling.classList.contains("part-title")) {
+                languageChapters[child.getAttribute("href").slice(3, 8)].push(item.previousElementSibling);
+                item.previousElementSibling.style.display = "none";
+            }
+            languageChapters[child.getAttribute("href").slice(3, 8)].push(item);
             item.style.display = "none";
         }
     });
 }
 
+let getCurrentLanguageChapter = () => {
+    let chapterItems = Array.from(document.getElementsByClassName("chapter-item expanded affix"));
+    for (let item of chapterItems) {
+        if (item.textContent == currentLanguage) {
+            return item;
+        }
+    }
+}
+
 hideEmptyChapterItems();
 
+let displayChapterItemsOfCurrentLanguage = () => {
+    let items = languageChapters[currentLanguage];
+    let first = items[0];
+    if (first) {
+       getCurrentLanguageChapter().insertAdjacentElement('afterend', first);
+    }
+    for (let item of items) {
+        first = first.insertAdjacentElement('afterend', item);
+        item.style.display = "inherit";
+    }
+}
+
 document.addEventListener('DOMContentLoaded', hideEmptyChapterItems);
+document.addEventListener("DOMContentLoaded", displayChapterItemsOfCurrentLanguage);
